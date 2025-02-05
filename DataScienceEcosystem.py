52eb0a0c6427ade30e05d7586609a31cb472977f
nadunhs25 <nadunhs25@gmail.com>
2025-02-05 12:40:19 +1100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summerized.
+
+# ### Some of the popular language that Data Scienctists use are:
+
+# 1. Python
+# 2. R
+# 3. Java
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+
+# 1. Numpy
+# 2. Pandas
+# 3. Matplotlib
+
+# ### Data Science Tools
+
+# 1. Matplotlib
+# 2. TensorFlow
+# 3. Tableau
+# 4. R
+# 5. Apache Spark
+# 6. Apache Hadoop
+
+# ### Below are a few examples of evaluating arithmatic expressions in python.
+
+# In[4]:
+
+
+(3*4)+5
+
+
+# In[5]:
+
+
+200/60
+
+
+# ### List popular language in Data Science
+
+# * Popular language for Data Science
+# * Popular libraries for Data Science
+# * Popular tools for Data science
+
+# In[ ]:
+
+
+
+
